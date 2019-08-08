df1231102cb4829d54c6a42fd8e86efd3e51cc76
“Rishi <YOUR@EMAIL.com>
2019-08-07 19:19:13 -0700

    can display all steps for directions and total time/distance

--- app/controllers/api/schedule_controller.rb
+++ app/controllers/api/schedule_controller.rb
@@ -4,9 +4,17 @@ class Api::ScheduleController < ApplicationController
   def google_maps
     current_location = "Granite Bay High School".tr(" ", "+")
     destination = "80 Powers Drive".tr(" ", "+")
-    @directions = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{current_location}d&destination=#{destination}&key=#{ENV["API_KEY"]}").parse
-    # @directions{'routes'}[1][4][0]{'html_instructions'}
-    # @directions{'routes'}{'legs'}{'steps'}[0]{'html_instructions'}
+    directions = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{current_location}d&destination=#{destination}&key=#{ENV["API_KEY"]}").parse
+    @total = HTTP.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{current_location}&destinations=#{destination}&key=#{ENV["API_KEY"]}").parse
+    # binding.pry
+    index = 0
+    @steps = []
+    while index < directions["routes"][0]["legs"][0]["steps"].count
+      @steps << directions["routes"][0]["legs"][0]["steps"][index]["html_instructions"]
+      index = index + 1
+    end
+    @total_time = total['rows'][0]['elements'][0]['duration']['text']
+    @total_distance = total['rows'][0]['elements'][0]['distance']['text']
     render 'index.json.jb'
   end
 
@@ -16,6 +24,7 @@ class Api::ScheduleController < ApplicationController
   #   render 'onebusaway.json.jb'
   # end
 
+#simple create for new trip
   def create
     @trip = Trip.new(
       current_location: params[:current_location],
@@ -24,7 +33,7 @@ class Api::ScheduleController < ApplicationController
     @trip.save
     render 'show.json.jb'
   end
-
+#sends google maps data to front end after user inputs location and destination
   def specialcreate
     @trip = Trip.new(
       current_location: params[:current_location],
@@ -34,7 +43,18 @@ class Api::ScheduleController < ApplicationController
 
     current_location = @trip.current_location.tr(" ", "+")
     destination = @trip.destination.tr(" ", "+")
-    @directions = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{current_location}d&destination=#{destination}&key=#{ENV["API_KEY"]}").parse
+    directions = HTTP.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{current_location}d&destination=#{destination}&key=#{ENV["API_KEY"]}").parse
+    total = HTTP.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{current_location}&destinations=#{destination}&key=#{ENV["API_KEY"]}").parse
+    index = 0
+    count = 1
+    @steps = []
+    while index < directions["routes"][0]["legs"][0]["steps"].count
+      @steps << "#{count}. #{directions["routes"][0]["legs"][0]["steps"][index]["html_instructions"]}"
+      index = index + 1
+      count = count + 1
+    end
+    @total_time = total['rows'][0]['elements'][0]['duration']['text']
+    @total_distance = total['rows'][0]['elements'][0]['distance']['text']
     render 'index.json.jb'
   end
 end
